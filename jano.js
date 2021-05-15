ad0bdfd842a5eef6e6c90afb247e47b08a4ebbd9
zizo128 <84239238+zizo128@users.noreply.github.com>
2021-05-15 19:44:58 +0300

    Update jano.js

--- jano.js
+++ jano.js
@@ -7392,7 +7392,7 @@ jano200.on("ready", () => {
 
 //////////////// tokn lera da bne 
 
-jano.login("");
+jano.login("ODQxNDYyOTE4NjgzNTU3ODg4.YJ_5kg.OmKwzuQujbknD95h6ugCTjEDNrU");
 jano2.login("");
 jano3.login("");
 jano4.login("");
