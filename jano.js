0baf3fc399b9abd805a6d2005e616f0a4fa25ddc
JANO <77622056+Devjano@users.noreply.github.com>
2021-10-27 06:18:50 -0700

    Update jano.js

--- jano.js
+++ jano.js
@@ -6,7 +6,7 @@ app.get("/", (request, response) => {
 });
 app.listen(process.env.PORT);
 setInterval(() => {
-  http.get(`https://working-petalite-face.glitch.me`);
+  http.get(``);
 }, 280000);
 ////////////////
 const Discord = require("discord.js");
